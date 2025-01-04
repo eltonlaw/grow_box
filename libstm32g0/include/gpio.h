9c3eaab04d3827c023e93bc97284836d72daaf60
Elton Law <eltonlaw296@gmail.com>
2025-01-03 23:37:33 -0500

    chore: refactor gpio utils

--- libstm32g0/include/gpio.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-typedef struct {
-} gpio_config_t;
-void gpio_open(gpio_config_t *config);
--- src/main.c
+++ src/main.c
@@ -15,17 +15,39 @@ void delay(uint32_t delay) {
     while ((uw_tick - tickstart) < delay);
 }
 
+/* Setup PA5 as output fro status pin */
+void gpio_init() {
+    volatile uint32_t tmpreg;
+
+    /* Enable GPIOA clock */
+    RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
+    /* Ensure clock is enabled by read op */
+    tmpreg = RCC->IOPENR & RCC_IOPENR_GPIOAEN;
+    (void) tmpreg;
+    /* Configure PA5 as output */
+    GPIOA->MODER &= ~GPIO_MODER_MODE5;
+    GPIOA->MODER |= GPIO_MODER_MODE5_0;
+    GPIOA->OTYPER &= ~GPIO_OTYPER_OT5;
+    GPIOA->OSPEEDR &= ~GPIO_OSPEEDR_OSPEED5;
+    GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED5_0;
+    GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD5;
+}
+
 int main(void) {
     /* 16Mhz internal clock, 1ms per systick tick */
+    // OS_Tick_Enable();
     SysTick_Config(16000);
     SystemCoreClockUpdate();
 
-    gpio_config_t gpio_cfg = {};
-    gpio_open(&gpio_cfg);
+    gpio_init();
     while (1) {
         GPIOA->BSRR = GPIO_BSRR_BS5;
-        delay(1000);
+        delay(500);
         GPIOA->BSRR = GPIO_BSRR_BR5;
-        delay(1000);
+        delay(500);
+        GPIOA->BSRR = GPIO_BSRR_BS5;
+        delay(5000);
+        GPIOA->BSRR = GPIO_BSRR_BR5;
+        delay(5000);
     }
 }
